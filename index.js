f57d9499a9623b99d19f06a93f57f3a273182efa
imveny <amineprojet7@gmail.com>
2021-06-22 14:52:37 +0200

    (dep): add mongodb, bcrypt, passport (feat): test data api

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
-const express = require("express")
-const app = express()
-const port = 3001
-app.get('/session/auth', (req, res) => {
-  res.send({ express: 'test' })
+const express = require("express");
+const app = express();
+const port = 3001;
+const MongoClient = require("mongodb").MongoClient;
+
+const db;
+MongoClient.connect("mongodb://localhost:27017", function (err, client) {
+  console.log("Connected successfully to server");
+  db = client.db("prod");
 });
-app.listen(port, () => console.log(`Listening on port ${port}`))
+
+app.use(express());
+
+app.get("/session/auth", (req, res) => {
+  const user = res.send({
+    username: "amine",
+    password: "test123",
+    error: "false",
+    success: "false",
+  });
+
+  if (!user) {
+    return res
+      .status(401)
+      .json({ message: "Invalid Authentication Credentials" });
+  }
+});
+app.get("/add/weight", (req, res) => {
+  res.send({ weight: "300kg" });
+});
+app.get("/list/weight", (req, res) => {
+  res.send({
+    list: ["200kg", "200kg"],
+    type: ["Caisse", "Bennes"],
+    data: ["Canettes", "PET"],
+  });
+});
+app.get("/session/error", (err, res) => {
+  if (typeof err === "string") {
+    return res.status(400).json({ message: err });
+  }
+
+  if (!res.status(500)) {
+    return res.status(500).json({ message: err.message });
+  }
+});
+app.listen(port, () => console.log(`Listening on port ${port}`));
