b4bf9d5aa474d862ee695fc2a40633630a8f919d
imveny <amineprojet7@gmail.com>
2021-06-23 14:37:30 +0200

    woops.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3001;
 
 app.use(express());
 app.get("/", (res, req) => {
-  res.send("it work.");
+  res.status(200).send("it work.");
 });
 app.get("/list/weight", (req, res) => {
   res.json({
