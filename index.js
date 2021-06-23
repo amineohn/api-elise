76a59bc04bdcdaeb98a562f197062f868ceb7862
imveny <amineprojet7@gmail.com>
2021-06-23 14:27:01 +0200

    update

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
-const MongoClient = require("mongodb").MongoClient;
-
-MongoClient.connect("mongodb://localhost:27017", function (err, client) {
-  client.db("prod");
-  if (client.db("prod")) {
-    console.log("Connected successfully to server");
-  } else {
-    console.log("can't connect to MongoDB");
-  }
-});
 
 app.use(express());
-
+app.get("/", (res, req) => {
+  res.send("it work.");
+});
 app.get("/list/weight", (req, res) => {
-  res.send({
-    list: ["200kg", "200kg"],
-    type: ["Caisse", "Bennes"],
-    data: ["Canettes", "PET"],
+  res.json({
+    weight: ["200kg"],
+    type: ["Caisse"],
+    deposit: ["Canettes"],
   });
 });
 
@@ -33,31 +25,14 @@ app.get("/session/error", (err, res) => {
 });
 
 // endpoints
-app.put("/session/auth", (req, res) => {});
-app.delete("/session/auth", (req, res) => {});
-app.post("/session/auth", (req, res) => {
-  const user = res.send({
-    username: "amine",
-    password: "test123",
-    error: "false",
-    success: "false",
+
+app.post("/add/weight/:number", (req, res) => {
+  res.status(200).json({
+    weight: "10",
+    type: "kg",
+    created: true,
+    value: "yes",
   });
-
-  if (!user) {
-    return res
-      .status(401)
-      .json({ message: "Invalid Authentication Credentials" });
-  }
 });
-
-app.post("/add/weight/:value", (req, res) => {
-  res
-    .status(200)
-    .send({ weight: "10", type: "kg", created: true, value: "yes" });
-});
-
-app.put("/session/create", (req, res) => {});
-app.delete("/session/create", (req, res) => {});
-app.post("/session/create", (req, res) => {});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
