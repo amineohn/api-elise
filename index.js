b30a70d20c83c8d4de811814d5d0b9850a940ad5
imveny <amineprojet7@gmail.com>
2021-06-24 15:18:11 +0200

    delete unused request use instanted => except function

--- index.js
+++ index.js
@@ -2,32 +2,49 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 const cors = require("cors");
-var db = require("./src/database/database");
+const db = require("./src/database/database");
 
 app.use(cors());
 app.use(express());
 app.use(express.urlencoded({ extended: true }));
-app.get("/", function (req, res) {
+
+app.get("/", (req, res) => {
   res.json({
     message: ["it work."],
   });
 });
-app.param(["number"], function (req, res, next, value) {
+
+/*app.param(["number"], (req, res, next, value) => {
+  db.run("INSERT INTO weight (weight) VALUES (?)", value);
+  res.json({
+    weight: value,
+  });
+  next();
+});*/
+
+app.param(["weight"], (req, res, next, value) => {
   db.run("INSERT INTO weight (weight) VALUES (?)", value);
   res.json({
     weight: value,
   });
   next();
 });
-app.post("/weight/:number", function (req, res, next, value) {
+app.param(["type"], (req, res, next, value) => {
+  db.run("INSERT INTO weight (type) VALUES (?)", value);
+  res.json({
+    type: value,
+  });
+  next();
+});
+
+app.post("/weight/:type/:weight", (req, res, next, value) => {
+  req.params["type"];
+  req.params["weight"];
+
   next.end();
 });
 app.get("/weight", (req, res) => {
-  db.run(
-    `CREATE TABLE IF NOT EXISTS weight (id INTEGER PRIMARY KEY AUTOINCREMENT, weight int)`
-  );
-  var params = [];
-  db.all("SELECT * from weight", params, (err, rows) => {
+  db.all("SELECT * from weight", (err, rows) => {
     if (err) {
       res.status(400).json({ error: err.message });
       return;
@@ -38,6 +55,7 @@ app.get("/weight", (req, res) => {
     });
   });
 });
-app.get("/list/weight", function (res, req) {});
+
+app.get("/list/weight", (res, req) => {});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
