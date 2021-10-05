ac4c04633fa3aa264bccc282bc7d00df4d697530
amineohn <amineprojet7@gmail.com>
2021-10-05 11:50:29 +0200

    code system generation.

--- src/app.js
+++ src/app.js
@@ -3,10 +3,27 @@ const cors = require('cors')
 const connection = require('./database/database')
 const port = process.env.PORT || 3001
 const app = express()
+const nodemailer = require('nodemailer')
+require('dotenv').config()
 app.use(cors())
 app.use(express())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+const server = false
+const transporter = nodemailer.createTransport({
+    host: server ? process.env.HOST : process.env.HOST_SERVER,
+    secureConnection: false,
+    port: process.env.PORT,
+    tls: {
+        ciphers: 'SSLv3',
+    },
+    auth: {
+        user: process.env.AUTH_EMAIL,
+        pass: process.env.PASSWORD,
+    },
+})
+
 app.get(`/`, (req, res) => {
     res.json({
         success: true,
@@ -16,6 +33,49 @@ app.get(`/`, (req, res) => {
 app.post(`/add`, (req, res) => {
     connection.query({
         sql: `INSERT INTO data (matter, type, weight) VALUES (?, ?, ?)`,
+        values: [req.body.matter, req.body.type, req.body.weight],
+    })
+    res.json({
+        weight: req.body.weight,
+        type: req.body.type,
+        matter: req.body.matter,
+    })
+})
+
+app.post(`/code`, (req, res) => {
+    connection.query('SELECT * from data', (err, rows) => {
+        if (err) {
+            res.status(400).json({ error: err.message })
+        }
+        const options = {
+            from: 'smkamine@outlook.fr',
+            to: 'amineprojet7@gmail.com',
+            subject: `Poids Fin de journée`,
+            text: `Liste des maitères: ${JSON.stringify(rows)}`,
+            html: `Liste des maitères: ${JSON.stringify(rows)}`,
+        }
+        req.body.code === 'generatedsuccess'
+            ? transporter.sendMail(options, (error, info) =>
+                  error
+                      ? console.log(error)
+                      : console.log('Message sent: ' + info.response)
+              )
+            : res.json({
+                  code: 'notfound',
+              })
+    })
+
+    res.json({
+        code: req.body.code,
+        weight: req.body.weight,
+        type: req.body.type,
+        matter: req.body.matter,
+    })
+})
+
+app.delete(`/delete`, (req, res) => {
+    connection.query({
+        sql: `DELETE FROM data WHERE weight LIMIT 1`,
         values: [req.body.matter, req.body.type, req.body.weight],
     })
     res.json({
